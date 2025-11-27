e583ac103b915a430b2537ec51e4ad6155f4c25a
2456Zebra <2456Zebra@gmail.com>
2025-11-26 18:04:06 -0800

    Add CompanySearch component for domain input

---
+++ components/CompanySearch.tsx
@@ -0,0 +1,9 @@
+export default function CompanySearch() {
+  return (
+    <input
+      type="text"
+      placeholder="Enter company domain (e.g. coca-cola.com)"
+      className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+}
